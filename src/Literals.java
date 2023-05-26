9e88619181c2555c9ade2979255edf32a476ca45
mohit <mohit.vaswani.work@gmail.com>
2023-05-26 19:19:27 -0400

    Basics

---
+++ src/Literals.java
@@ -0,0 +1,9 @@
+public class Literals {
+    public static void main(String[] args){
+        System.out.println("Here in Literals");
+//        byte k = 127;
+        float mv = 9.09890f;
+        System.out.println(mv);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static int sum(int a,int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.printf("Hello world!");
+        int m = sum(5,6);
+        System.out.println(m);
+    }
+}
